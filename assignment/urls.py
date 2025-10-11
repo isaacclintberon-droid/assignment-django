b375bc07594c07d0cddbbfb84ab8fd63889c92b0
isaacclintberon-droid <isaacclint.beron@evsu.edu.ph>
2025-10-12 03:33:03 +0800

    Added Library app with Author, Category, and Book models + templates

--- assignment/urls.py
+++ assignment/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),  # home, about
-    path('announcements/', include('announcements.urls')),  # announcements
+    path('', include('pages.urls')),
+    path('announcements/', include('announcements.urls')),
+    path('library/', include('library.urls')),
 ]
